42e28915ecbb42c3eb1e9e761d8d2217ba6d473f
Alex C-G <alexcg@outlook.com>
2023-01-25 16:02:43 +0100

    refactor: readline, rich, interactive mode
    Signed-off-by: Alex C-G <alexcg@outlook.com>

---
+++ app.py
@@ -0,0 +1,36 @@
+import readline
+import subprocess
+
+import click
+from rich import print
+from rich.prompt import Prompt
+
+
+def get_response(query):
+    response = subprocess.run(
+        ['gpto', '-p', f'{query}?'],
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    ).stdout.strip()
+    output = f'[bold red]gpt3[/bold red]: {str(response)}'
+
+    return output
+
+
+@click.command()
+# @click.option('--model', default='text-davinci-003')
+# @click.option('--temperature', default=0.9)
+@click.argument('query', nargs=-1)
+def main(query: str, model: str, temperature: float):
+    if len(query) == 0:
+        while True:
+            query = Prompt.ask('[bold green]User[/bold green]')
+            print(get_response(query))
+
+    else:
+        query = ' '.join(query)
+        print(get_response(query))
+
+
+if __name__ == '__main__':
+    main()
